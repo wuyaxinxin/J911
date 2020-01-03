3971d71c2fa00478b26094420958cf4e5a888364
wuyaxin <2749475736@qq.com>
2020-01-03 17:30:09 +0800

    第一次改变

---
+++ KeShe/src/xiaochengxu/Student.java
@@ -0,0 +1,13 @@
+package xiaochengxu;
+
+public class Student {
+static String name;
+int age;
+@Override
+public String toString() {
+	return "Student [name=" + name + ", age=" + age + "]";
+}
+
+
+
+}
---
+++ KeShe/src/xiaochengxu/XiaoChengXu.java
@@ -0,0 +1,16 @@
+package xiaochengxu;
+
+import java.util.Scanner;
+
+public class XiaoChengXu {
+public static void main(String[] args) {
+	System.out.println("欢迎使用小程序助你破产");
+	Scanner input =new Scanner(System.in);
+	while(true) {
+		System.out.println("可选择的操作有：\n1,登陆\n2,注册\n3,退出");
+		String tag=input.next();
+		
+	}
+	
+}
+}
